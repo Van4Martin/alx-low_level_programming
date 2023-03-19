8073d382ca127cd7a8f6fdd65cb4b06ea0d2fcc0
Van4Martin <127011745+Van4Martin@users.noreply.github.com>
2023-03-19 22:07:04 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 */
 int main(void)
 {
-    char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-    puts(str1);
-    return 1;
+        char str1[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    
+        puts(str1);
+        return (1);
 }
