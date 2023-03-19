47855b900bc2e52e5d2874b15c0a01599ed9de15
Martin <van4martin@gmail.com>
2023-03-18 18:03:09 -0700

    "Betty"

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha1 = 'a';
+	char alpha2 = 'A';
+
+	while (alpha1 <= 'z')
+	{
+		putchar(alpha1);
+		alpha1 = alpha1 + 1;
+	}
+	while (alpha2 <= 'Z')
+	{
+		putchar(alpha2);
+		alpha2 = alpha2 + 1;
+	}
+	{
+		putchar('\n');
+	}
+}
