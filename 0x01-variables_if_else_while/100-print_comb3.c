adeed7ae36ac9103747c5706560ef251f281a129
Martin <van4martin@gmail.com>
2023-03-19 16:23:05 -0700

    1000th

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n < 9; n++)
 	{
 		for (m = 0; m < 10; m++)
 		{
 			putchar((n % 10) + '0');
 			putchar((m % 10) + '0');
-			if (n == 9 && m == 9)
+			if (n == 8 && m == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
