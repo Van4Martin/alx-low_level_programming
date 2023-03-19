26ad9cee645032d157b399230bf51a5aed3210a2
Van4Martin <127011745+Van4Martin@users.noreply.github.com>
2023-03-19 22:00:06 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* main - Entry Point
+*
+* Return: ALways 0 (Success)
 int main(void)
 {
     char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
