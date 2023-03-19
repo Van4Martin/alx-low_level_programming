78705bb6149f28f8e7937c99c2976b4b549bb955
Martin <van4martin@gmail.com>
2023-03-19 07:24:23 -0700

    "It worked"

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
