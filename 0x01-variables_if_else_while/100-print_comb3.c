4dded11a2863b8ef5a085427647abd33e952a44e
Martin <van4martin@gmail.com>
2023-03-19 16:13:27 -0700

    "Still working"

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			putchar((n % 10) + '0');
+			putchar((m % 10) + '0');
+			if (n == 9 && m == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
