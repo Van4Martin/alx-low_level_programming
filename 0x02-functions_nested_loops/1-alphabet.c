e72cb001eadc511106d3ee2d987ce8ebae36488e
Martin <van4martin@gmail.com>
2023-03-21 15:17:40 -0700

    " Main"" `

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n = 'a';
+
+	while(n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+	return (0);
+}
