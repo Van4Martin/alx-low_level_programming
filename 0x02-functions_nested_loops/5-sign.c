1488ee4caadf0894ec85deadf93e4df71e28fa68
Martin <van4martin@gmail.com>
2023-03-21 19:18:05 -0700

    Good

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Print sign of character
+ * @n: number whose sign to be printed
+ *
+ * Return: 1 or 0
+ * result, dependent
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (10);
+		_putchar(75);
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar(0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar(77);
+	}
+}
