4eb8218777b0ec7aebf45974ca65b78617c17dfe
Martin <van4martin@gmail.com>
2023-03-22 15:56:48 -0700

    'rturn lastDigit"

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Computes and print the last digit of int num
+ * @num: Integer whose last digit is to be computed
+ * Return: lastDigit (Success)
+ */
+int print_last_digit(int num)
+{
+	int lastDigit;
+
+	lastDigit = num % 10;
+	return (lastDigit);
+}
+
