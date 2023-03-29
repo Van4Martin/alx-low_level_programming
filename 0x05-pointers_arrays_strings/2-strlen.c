661e7b1a0b5d603012fa6f2d76b8d992949967ee
Martin <van4martin@gmail.com>
2023-03-28 18:50:18 -0700

    "Lenth computation"

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Computes the lenth of a string
+ * @s: string whose lenth is to be computed
+ *
+ * Return: Lenth of string (Success)
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = *(s + 1);
+	return (len);
+}
