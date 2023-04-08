00e8c96a08b05e846c4ff979133de5240605a459
Martin <van4martin@gmail.com>
2023-04-08 06:32:56 -0700

    iiii

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 char *_strcat(char *dest, char *src)
 {
 	int i,  j;
-	
+
 	i = 0;
-	while(*(dest + i) != '\0')
+	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while(j >= 0)
+	while (j >= 0)
 	{
 		*(dest + i) = *(src + j);
 		if (*(src + j) == '\0')
