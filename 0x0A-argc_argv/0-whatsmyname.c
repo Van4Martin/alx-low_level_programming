bb5e08be20459314e0361849bf4a3c869c2b7f3e
Martin <van4martin@gmail.com>
2023-04-08 10:29:35 -0700

    iiiii

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: argument vector(strings passed to main from the command line)
  * Return: Always 0 (Sucess)
  */
-int main(int argc, char *argv[])
+int main(int argc  __attribute__((unused)), char *argv[])
 {
 	int i;
 
