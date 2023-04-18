c93fb2bdfb0edad225aa4e5789c6e13c9bff9da7
Martin <van4martin@gmail.com>
2023-04-17 19:07:13 -0700

    "Structures"

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * init_dog -
+ * @d:
+ * @name:
+ * @age:
+ * @owner:
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = {
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _dog_h
+#define _dog_h
+
+/**
+ * struct dog - Structure for a dog
+ * @name: Name of dog
+ * @age: Age of the dog
+ * @owner: Owner's name
+ *
+ * Description: This is a simple dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
