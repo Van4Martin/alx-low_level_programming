28198ea731cfa8ee61ff048863b09f62d7917870
Martin <van4martin@gmail.com>
2023-04-18 16:55:40 -0700

    "Correction"

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - Creates a new dog
+ * @name: hrhrfh
+ * @age: yhfshfhsf
+ * @owner: jdsjhsfjsf
+ *
+ * Return: Nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int ln1, ln2;
+	
+	ln1 = strlen(name) + 1;
+	ln2 = strlen (owner) + 1;
+	dog = malloc(sizeof(dog_t));
+	if ( dog == NULL)
+	{
+		return (NULL);
+	}
+	dog->name = malloc(sizeof(char) * ln1);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		return (NULL);
+	}
+	dog->owner =  malloc(sizeof(char) * ln2);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		return (NULL);
+	}
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
+}
