a2f2b18053463c71190cbb605cef58db79b36a37
Martin <van4martin@gmail.com>
2023-04-18 16:59:35 -0700

    "Correction"

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int ln1, ln2;
-	
+
 	ln1 = strlen(name) + 1;
-	ln2 = strlen (owner) + 1;
+	ln2 = strlen(owner) + 1;
 	dog = malloc(sizeof(dog_t));
-	if ( dog == NULL)
+	if (dog == NULL)
 	{
 		return (NULL);
 	}
