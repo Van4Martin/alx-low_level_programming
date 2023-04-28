575bda35e1458ff9c193ee5660431da2a1498720
Martin <van4martin@gmail.com>
2023-04-27 19:07:02 -0700

    "Betty errors checked and updated""

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,14 @@
  */
 void free_list(list_t *head)
 {
-        list_t *current_node;
+	list_t *current_node;
 
-        while (head != NULL)
-        {
-                current_node = head;
-                head = head->next;
-                free(current_node->str);
-                free(current_node);
-        }
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
 }
 
