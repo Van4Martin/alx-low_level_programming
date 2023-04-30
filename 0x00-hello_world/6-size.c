843a0c4e8505f3254584494e33509fdaffb3ddf4
Martin <van4martin@gmail.com>
2023-04-30 10:19:04 -0700

    corection for '6-size.c' in the directory 0x00-hello_world, all files now working and updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 
-	printf("Size of a char: %d bite(s)\n", sizeof(char));
-	printf("Size of an int: %d bite(s)\n", sizeof(int));
-	printf("Size of a long int: %d bite(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d bite(s)\n", sizeof(long long int));
-	printf("Size of a float: %d bite(s)\n", sizeof(float));
+	printf("Size of a char: %lu bite(s)\n", sizeof(char));
+	printf("Size of an int: %lu bite(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bite(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bite(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bite(s)\n", sizeof(float));
 	return (0);
 }
 
