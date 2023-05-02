9bcceaf485bfcf5360d7f7bc7b9bd6e181c3fc79
Martin <van4martin@gmail.com>
2023-05-01 17:31:53 -0700

    Working hard

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in the liat
+ * @h: pointer to head
+ *
+ * Return: No of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	
+	while (h != NULL);
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
